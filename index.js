89501acc2a10f509d3ec07c0ecc45cbb3ee83ec3
Rio-Lv <wong.rogerio@gmail.com>
2022-05-07 14:56:10 +0100

    change to ts

--- index.js
+++
@@ -1,5 +0,0 @@
-export const test = () => {
-  console.log("This is a message from the demo package");
-};
-
-export default "some text e.g Cat";
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const test = (): string => {
+  console.log("gluten tart");
+  return "hello Therre";
+};
